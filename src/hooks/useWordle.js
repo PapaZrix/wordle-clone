07b4e06488f4f3d6e871482a06f8bffbdefe807e
PapaZrix <zrilic313@gmail.com>
2023-07-11 16:04:22 +0200

    Add functions to useFetch hook

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+export default function useWordle(solution) {
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([...Array(6)])
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    const formatGuess = () => {
+        const solutionLetters = [...solution]
+        const formatedGuess = [...currentGuess].map(l => {
+            return { key: l, color: 'gray' }
+        })
+
+        formatedGuess.forEach((l, i) => {
+            if (solution[i] === l.key) {
+                formatedGuess[i].color = 'green'
+            }
+        })
+
+        formatedGuess.forEach((l, i) => {
+            if (solutionLetters.includes(l.key) && l.color !== 'green') {
+                formatedGuess[i].color = 'yellow'
+            }
+        })
+
+        return formatedGuess
+    }
+
+    const addNewGuess = (formatedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
+
+        setGuesses(prevGuesses => {
+            const newGuess = [...prevGuesses]
+            newGuess[turn] = formatedGuess
+            return newGuess
+        })
+
+        setTurn(prevTurn => {
+            return prevTurn + 1
+        })
+
+        setCurrentGuess('')
+    }
+
+    const handleKeyup = ({ key }) => {
+        if (key === 'Enter') {
+            if (turn > 5) {
+                console.log('No more guesses')
+                return
+            }
+
+            if (guesses.includes(currentGuess)) {
+                console.log('You have already tried that word')
+                return
+            }
+
+            if (currentGuess.length !== 5) {
+                console.log('Please enter full word')
+                return
+            }
+
+            const formatedGuess = formatGuess()
+            addNewGuess(formatedGuess)
+        }
+
+        if (key === 'Backspace') {
+            setCurrentGuess(prev => prev.slice(0, -1))
+            return
+        }
+
+        if (/^[A-Za-z]$/.test(key)) {
+            if (currentGuess.length < 5) {
+                setCurrentGuess(prev => prev + key)
+            }
+        }
+    }
+
+    const giveUp = () => {
+        setTurn(6)
+        setIsCorrect(false)
+    }
+
+    const newGame = () => {
+        setGuesses([...Array(6)])
+        setCurrentGuess('')
+        setTurn(0)
+        setIsCorrect(false)
+    }
+
+    return { giveUp, newGame, handleKeyup, turn, currentGuess, guesses, isCorrect }
+}
