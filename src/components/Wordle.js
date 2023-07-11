66be0a33e9b1d5f3b32db8256c723e37c1d9e6ac
PapaZrix <zrilic313@gmail.com>
2023-07-11 16:08:43 +0200

    Import hooks and set up event listener for keystrokes

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react"
 import Grid from "./Grid"
+import useFetch from "../hooks/useFetch"
+import useWordle from "../hooks/useWordle"
 
 export default function Wordle() {
+    const { solution, getSolution } = useFetch()
+    const { giveUp, newGame, handleKeyup, turn, isCorrect, currentGuess, guesses } = useWordle(solution)
+    const [savedSolution, setSavedSolution] = useState('')
+
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyup)
+
+        return () => window.removeEventListener('keyup', handleKeyup)
+
+    }, [handleKeyup])
+
     return (
         <>
             <header>
